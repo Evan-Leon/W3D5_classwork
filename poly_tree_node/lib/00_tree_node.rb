3fb516eeb841e45c3e8fedfbfef48d75d4dfb9b8
Evan-Leon <evan.leon.j@gmail.com>
2021-03-12 15:41:37 -0500

    Defined parent setter

--- poly_tree_node/lib/00_tree_node.rb
+++ poly_tree_node/lib/00_tree_node.rb
@@ -1,21 +1,23 @@
 class PolyTreeNode
     attr_reader :parent, :children, :value
-
+    attr_writer :children
+    
     def initialize(value = nil)
         @value = value
         @parent = nil
         @children = []
     end
 
-    def parent=(node)
-        parent = node
-
-        # if parent.nil
-
+    def parent=(parent_node)
+        # parent_node ||= 
+        @parent = parent_node
+        if !parent_node.children.include?(self) 
+            parent_node.children << self 
+        end
     end
 
-    def children
-        #change
-    end
+    # def children
+    #     #change
+    # end
 
 end
